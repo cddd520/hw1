478cf8606bd094cb0a70f1488696bf5fe217744a
Blair Chen <blairche@usc.edu>
2025-09-10 04:23:33 +0000

    add code for split

--- split.cpp
+++ split.cpp
@@ -16,8 +16,28 @@ the function below should be the only one in this file.
 
 void split(Node*& in, Node*& odds, Node*& evens)
 {
-  /* Add code here */
-// WRITE YOUR CODE HERE
+  // base case
+  if (in == nullptr)
+  {
+    return;
+  }
+  // parse the big problem into smaller problem, put the head into next position
+  Node* temp = in;
+  in = temp -> next;
+  // recursive through smaller problem
+  split(in, odds, evens);
+  // add the head of odds or evens into front of these two lists
+  if (temp->value % 2 == 1)
+  {
+    temp->next = odds;
+    odds = temp;
+  }
+  else
+  {
+    temp->next = evens;
+    evens = temp;
+  }
+  
 }
 
 /* If you needed a helper function, write it here */
--- test_split.cpp
+++ test_split.cpp
@@ -10,8 +10,65 @@ g++ split.cpp test_split.cpp -o test_split
 */
 
 #include "split.h"
+#include "iostream"
+using namespace std;
 
 int main(int argc, char* argv[])
 {
+  // create test case
+
+  Node* node1 = new Node(1, nullptr);
+  Node* node2 = new Node(2, nullptr);
+  node1 -> next = node2;
+  Node* node3 = new Node(3, nullptr);
+  node2 -> next = node3;
+  Node* node4 = new Node(4, nullptr);
+  node3 -> next = node4;
+  Node* node5 = new Node(5, nullptr);
+  node4 -> next = node5;
+  // print the original list
+  Node* temp = node1;
+  while (temp != nullptr)
+  {
+    cout << temp -> value << endl;
+    temp = temp -> next;
+  }
+  // create node for odds and evens
+  Node* odds = nullptr;
+  Node* evens = nullptr;
+
+  cout << "----------------" << endl;
+  // use the function split
+  split(node1, odds, evens);
+
+  // print odds and evens
+  Node* tempOdd = odds;
+  while (tempOdd != nullptr)
+  {
+    cout << tempOdd -> value << endl;
+    tempOdd = tempOdd -> next;
+  }
+  cout << "----------------" << endl;
+  Node* tempEven = evens;
+  while (tempEven != nullptr)
+  {
+    cout << tempEven -> value << endl;
+    tempEven = tempEven -> next;
+  }
+
+  // delete and release memory
+  while (odds != nullptr)
+  {
+    Node* temp = odds;
+    odds = odds -> next;
+    delete temp;
+  }
+  while (evens != nullptr)
+  {
+    Node* temp = evens;
+    evens = evens -> next;
+    delete temp;
+  }
+
 
 }
